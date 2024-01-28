3c390f1b48c16c778dec50e87debbd56686fd5bb
Nils Kriedner <nkriedner@googlemail.com>
2024-01-28 12:50:39 +0100

    Add functionality to switch between modal profiles + refactor js code

--- js/script.js
+++ js/script.js
@@ -1,22 +1,68 @@
 const modalBgOverlay = document.querySelector(".modal-bg-overlay");
+const modalContainer = document.querySelector(".modal-container");
+
+function createModalHTML(randomUserData, userData, cardIndex) {
+    // create the modal with user data content:
+    const modalHTML = `
+                <div class="modal">
+                    <span class="modal-x">X</span>
+                    <img class="modal-img" src="${userData.picture.medium}" />
+                    <h2 class="modal-name">${userData.name.first} ${userData.name.last}</h2>
+                    <span class="modal-email">${userData.email}</span>
+                    <span class="modal-city">${userData.location.city}</span>
+                    <hr />
+                    <span class="modal-phone">${userData.phone}</span>
+                    <span class="modal-address">${userData.location.street.number} ${userData.location.street.name}, ${userData.location.state} ${userData.location.postcode}</span>
+                    <span class="modal-birthday">Birthday: ${userData.dob.date}</span>
+                    <span class="previous-profile">&lAarr;</span>
+                    <span class="next-profile">&rAarr;</span>
+                </div>
+            `;
+    // show the modal:
+    modalContainer.innerHTML = modalHTML;
+    modalBgOverlay.style.display = "block";
+    modalContainer.style.display = "flex";
+
+    // MODAL CLOSING FUNCTIONALITY
+    const modalCloseX = document.querySelector(".modal-x");
+    modalCloseX.addEventListener("click", () => {
+        modalBgOverlay.style.display = "none";
+        modalContainer.style.display = "none";
+    });
+
+    // NEXT & PREVIOUS PROFILE FUNCTIONALITY
+    const previousProfileBtn = document.querySelector(".previous-profile");
+    const nextProfileBtn = document.querySelector(".next-profile");
+
+    previousProfileBtn.addEventListener("click", () => {
+        // define next profile-index (add 1):
+        if (cardIndex === 0) {
+            cardIndex = 11;
+        } else {
+            cardIndex--;
+        }
+        // create html for next profile modal
+        return createModalHTML(randomUserData, randomUserData[cardIndex], cardIndex);
+    });
+    nextProfileBtn.addEventListener("click", () => {
+        // define next profile-index (add 1):
+        if (cardIndex === 11) {
+            cardIndex = 0;
+        } else {
+            cardIndex++;
+        }
+        // create html for next profile modal
+        return createModalHTML(randomUserData, randomUserData[cardIndex], cardIndex);
+    });
+}
 
 async function getRandomUsers() {
     const response = await fetch("https://randomuser.me/api?results=12&nat=us");
     const randomUserJson = await response.json();
     const randomUserData = randomUserJson.results;
 
-    // const user1 = randomUserData.results[0];
-    // console.log(user1);
-    // console.log(user1.picture.medium);
-    // console.log(user1.name.first);
-    // console.log(user1.name.last);
-    // console.log(user1.email);
-    // console.log(user1.location.city);
-
     let cardContent = "";
     randomUserData.map((user, index) => {
-        // console.log(user);
-        // console.log(index);
         const card = `
             <div class="employee-card" data-index="${index}">
                 <img
@@ -31,17 +77,15 @@ async function getRandomUsers() {
                 </div>
             </div>
         `;
-        // console.log(card);
         const cardContainer = document.querySelector(".card-container");
         cardContent = cardContent + card;
         cardContainer.innerHTML = cardContent;
     });
     const employeeCards = document.querySelectorAll(".employee-card");
-    // console.log(employeeCards);
+
     // loop through each card and add a click event listener
     employeeCards.forEach(function (card) {
         card.addEventListener("click", function (e) {
-            // do something when the card is clicked:
             // get the card's index from the data-index attribute (on .employee-card & .employee-infos):
             let cardIndex;
             if (e.target.className != "employee-card") {
@@ -49,39 +93,9 @@ async function getRandomUsers() {
             } else {
                 cardIndex = parseInt(e.target.getAttribute("data-index"));
             }
-            // console.log("You clicked a card:", cardIndex, e.target);
             const userData = randomUserData[cardIndex];
-            console.log(userData);
 
-            // create the modal with user data content
-            const modalHTML = `
-                <div class="modal">
-                    <span class="modal-x">X</span>
-                    <img class="modal-img" src="${userData.picture.medium}" />
-                    <h2 class="modal-name">${userData.name.first} ${userData.name.last}</h2>
-                    <span class="modal-email">${userData.email}</span>
-                    <span class="modal-city">${userData.location.city}</span>
-                    <hr />
-                    <span class="modal-phone">${userData.phone}</span>
-                    <span class="modal-address">${userData.location.street.number} ${userData.location.street.name}, ${userData.location.state} ${userData.location.postcode}</span>
-                    <span class="modal-birthday">Birthday: ${userData.dob.date}</span>
-                </div>
-            `;
-            // show the modal
-            // console.log(modalHTML);
-            const modalContainer = document.querySelector(".modal-container");
-            modalContainer.innerHTML = modalHTML;
-            modalBgOverlay.style.display = "block";
-            modalContainer.style.display = "flex";
-
-            // MODAL FUNCTIONALITY
-            const modalCloseX = document.querySelector(".modal-x");
-            modalCloseX.addEventListener("click", () => {
-                console.log("click");
-                modalBgOverlay.style.display = "none";
-                modalContainer.style.display = "none";
-            });
-            // console.log(modalBgOverlay);
+            createModalHTML(randomUserData, userData, cardIndex);
         });
     });
 
@@ -117,9 +131,6 @@ document.querySelector("body").addEventListener("click", (e) => {
 });
 
 // TODOS:
-// + Employees can be filtered by name
-// + Functionality has been added to switch back and forth between employees
-//   when the detail modal window is open
 // + Directory has been styled so that all the major elements are in place and
 //   roughly matches the mockups
 // + Change and update birthday date format
